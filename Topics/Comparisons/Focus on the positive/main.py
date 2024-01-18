fe7ea45ca779cffa1f739ef26b973fd739d5ac76
Asher Downs <138610949+AsherDowns@users.noreply.github.com>
2024-01-17 20:57:46 -0500

    Delete Topics directory

--- Topics/Comparisons/Focus on the positive/main.py
+++
@@ -1,5 +0,0 @@
-# Please don't modify this code
-# `a` stores an input value
-a = int(input().strip())
-
-print(a > 0)
--- Topics/Declaring a function/Miles away/main.py
+++
@@ -1,5 +0,0 @@
-def mi_to_km(miles):
-    km = miles * 1.609
-    return(km)
-
-mi_to_km(100)
